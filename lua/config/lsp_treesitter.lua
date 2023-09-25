0bcd9aa869726f2ad33bbf5c849ed04d3cd42e85
Rikuto13ten <rikuto1367@gmail.com>
2023-09-25 22:16:25 +0900

    update

---
+++ lua/config/lsp_treesitter.lua
@@ -0,0 +1,39 @@
+local language = {
+  'lua',
+  'kotlin',
+}
+
+-- treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    language
+  },
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true
+  },
+  autotag = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = 1000,
+  },
+  context_commentstring = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+    },
+  }
+}
